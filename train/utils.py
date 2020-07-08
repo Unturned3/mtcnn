1b0978a743a88a47d4ddc2ef69d1d3e3d0e13766
Unturned3 <richardyusen@126.com>
2020-07-08 17:52:29 +0800

    changed dataloader code for using custom dataset

--- train/utils.py
+++ train/utils.py
@@ -1,15 +1,9 @@
-"""
-
-"""
-
-
-# Built-in
 import os
 from glob import glob
-
-# Libs
 import numpy as np
 from tqdm import tqdm
+
+# skimage
 from skimage import io, color
 import skimage.transform as sktrsfm
 from sklearn.metrics import precision_score, recall_score
@@ -18,96 +12,91 @@ from sklearn.metrics import precision_score, recall_score
 import torch
 import torch.nn.functional as F
 from torch.utils import data
-
 import torchvision
 from torchvision import transforms
 
-# Own modules
+# for parsing XML image annotations
+import xml.etree.ElementTree as ET
 
+def xml_to_numpy(anno_path):
+    
+    anno_names = sorted(glob(os.path.join(anno_path, '*.xml')))
+    
+    for ca in tqdm(anno_names):
+        
+        root = ET.parse(ca).getroot().find('object').find('bndbox')
+        
+        bb = np.zeros(4)
+        bb[0] = int(root.find('xmin').text)
+        bb[1] = int(root.find('ymin').text)
+        bb[2] = int(root.find('xmax').text)
+        bb[3] = int(root.find('ymax').text)
+        bb = bb.astype('int64')
+        np.save(os.path.splitext(ca)[0]+'.npy', bb)
+    
+    print('done!')
 
-class UTK_dataset(data.Dataset):
-    def __init__(self, img, age, bbox, prob, landmarks, trsfm=None):
-        self.img, self.age = img, age
-        self.bbox, self.prob, self.landmarks = bbox, prob, landmarks
+class CIVDS(data.Dataset):  # CIVDS: Children in Vehicles Dataset
+    
+    # img: children image
+    # prob: whether the image contains a child or not
+    # bbox: bounding box of the child in the image
+    def __init__(self, img, prob, bbox, trsfm=None):
+        self.img = img
+        self.prob = prob
+        self.bbox = bbox
         self.transforms = trsfm
     
     def __len__(self):
         return len(self.img)
     
     def __getitem__(self, idx):
-        img, age = self.img[idx], self.age[idx]
-        bbox, prob, landmarks = self.bbox[idx], self.prob[idx], self.landmarks[idx]
-        if self.transforms:
+        img, prob, bbox = self.img[idx], self.age[idx], self.bbox[idx]
+        if self.transforms is not None:
             img = self.transforms(img)
-        #return img, torch.tensor(age), torch.tensor(bbox), torch.tensor(prob), torch.tensor(landmarks)
-        return img, torch.tensor(age), torch.tensor(bbox), torch.tensor(prob), torch.tensor(landmarks)
+        return img, torch.tensor(prob), torch.tensor(bbox)
 
 
-def get_images(parent_path, age_thresh=(6, 18, 25, 35, 60), valid_percent=0.2, resize_shape=(32, 32), label_mode=False):
+def get_images(img_path, anno_path, valid_percent=0.1, resize_shape=(12, 12)):
     
-    # only *.chip.jpg is supported; No facial bbox, landmark, and probs provided for other images.
-    img_files = sorted(glob(os.path.join(parent_path, '*.chip.jpg')))
+    scale = resize_shape[0] / 400 # assuming that original image size is 400x400
     
-    imgs, ages, fn, bbox, prob, landmarks = [], [], [], [], [], [] # fn: file names
+    file_names = sorted(glob(os.path.join(img_path, '*.jpg')))
+    anno_names = sorted(glob(os.path.join(anno_path, '*.xml')))
     
-    ign_list = [] # list of files that were ignored
+    imgs, probs, bboxes = [], [], []
     
-    age_thresh = [-1, *age_thresh, 200]
-    
-    for img_file in tqdm(img_files):
+    for cur_file, cur_anno in tqdm(zip(file_names, anno_names)):
         
-        if label_mode == False:
-            # read face bbox, prob, and landmarks for each image
-            t_bbox = np.load(img_file+".bbox.npy")
-            t_prob = np.load(img_file+".prob.npy")
-            t_landmarks = np.load(img_file+".landmarks.npy")
-
-            if np.array_equal(t_bbox, [0,0,0,0]):
-                ign_list.append(os.path.basename(img_file))
-                continue # ignore this image; do not load
-
-            bbox.append(t_bbox[0])
-            prob.append(t_prob[0])
-            landmarks.append(t_landmarks[0])
+        img = io.imread(cur_file)
         
-        img = io.imread(img_file)
         if img.shape[-1] == 1:
             img = color.grey2rgb(img)
         
-        age = int(os.path.splitext(os.path.basename(img_file))[0].split('_')[0])
-        img = sktrsfm.resize(img, resize_shape, anti_aliasing=True, preserve_range=True)[..., :3]
+        age = os.path.splitext(os.path.basename(cur_file))[0].split('_')[0]
         
+        if age == 'n':
+            probs.append(0)
+            bboxes.append(np.array([0,0,0,0], dtype='int64'))
+        else:
+            probs.append(1)
+            a_bb = np.load(cur_anno)
+            b_bb = np.array([int(round(k*scale)) for k in a_bb], dtype='int64')
+            bboxes.append(b_bb)
+            
+        img = sktrsfm.resize(img, resize_shape, anti_aliasing=True, preserve_range=True)[..., :3]
         imgs.append(torchvision.transforms.ToPILImage()(img.astype(np.uint8)))
-        fn.append(img_file)
-
-        for cnt, (lb, ub) in enumerate(zip(age_thresh[:-1], age_thresh[1:])):
-            if lb < age <= ub:
-                ages.append(cnt)
-                break
-    
-    # temporarily disable randomization for dataset
     
     rand_idx = np.random.permutation(np.arange(len(img_files)))
     imgs = [imgs[a] for a in rand_idx]
-    ages = [ages[a] for a in rand_idx]
-    fn = [fn[a] for a in rand_idx]
-    bbox = [bbox[a] for a in rand_idx]
-    prob = [prob[a] for a in rand_idx]
-    landmarks = [landmarks[a] for a in rand_idx]
-    
-    
-    # debug printout
-    print("Ignored images: ")
-    for s in ign_list:
-        print(s)
+    probs = [probs[a] for a in rand_idx]
+    bboxes = [bboxes[a] for a in rand_idx]
     
     vn = int(np.floor(len(imgs) * valid_percent))
-    if label_mode == False:
-        return imgs[vn:], ages[vn:], fn[vn:], bbox[vn:], prob[vn:], landmarks[vn:], imgs[:vn], ages[:vn], fn[:vn], bbox[:vn], prob[:vn], landmarks[:vn]
-    else:
-        return imgs, ages, fn
+    return imgs[:vn], probs[:vn], bboxes[:vn], imgs[vn:], probs[vn:], bboxes[vn:]
 
 
+"""
 def f1_score(truth, pred, eval_class):
     def binarize(l, ref):
         l_new = np.zeros_like(l)
@@ -124,4 +113,4 @@ def f1_score(truth, pred, eval_class):
     recall = recall_score(truth, pred)
     f1 = 2 * (precision * recall) / (precision + recall)
     return precision, recall, f1
-
+"""
